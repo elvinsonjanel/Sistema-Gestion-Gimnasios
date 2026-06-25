c6b2ed49f47bcf16bdae86576f17f58cd4fbffc8
Juan Andres <juan252423222@gmail.com>
2026-06-24 20:14:08 -0400

    creacion modulo registrar membresias

---
+++ GoFitnessGym/modulo_de_registrar_membresias.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace GoFitnessGym
+{
+    partial class modulo_de_registrar_membresias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_precio = new System.Windows.Forms.Label();
+            this.lbl_duracion = new System.Windows.Forms.Label();
+            this.lbl_nombre = new System.Windows.Forms.Label();
+            this.lbl_descripcion = new System.Windows.Forms.Label();
+            this.lbl_subtitulo = new System.Windows.Forms.Label();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_precio
+            // 
+            this.lbl_precio.AutoSize = true;
+            this.lbl_precio.Location = new System.Drawing.Point(433, 275);
+            this.lbl_precio.Name = "lbl_precio";
+            this.lbl_precio.Size = new System.Drawing.Size(37, 13);
+            this.lbl_precio.TabIndex = 0;
+            this.lbl_precio.Text = "Precio";
+            // 
+            // lbl_duracion
+            // 
+            this.lbl_duracion.AutoSize = true;
+            this.lbl_duracion.Location = new System.Drawing.Point(420, 32);
+            this.lbl_duracion.Name = "lbl_duracion";
+            this.lbl_duracion.Size = new System.Drawing.Size(50, 13);
+            this.lbl_duracion.TabIndex = 1;
+            this.lbl_duracion.Text = "Duracion";
+            this.lbl_duracion.Click += new System.EventHandler(this.lbl_duracion_Click);
+            // 
+            // lbl_nombre
+            // 
+            this.lbl_nombre.AutoSize = true;
+            this.lbl_nombre.Location = new System.Drawing.Point(17, 32);
+            this.lbl_nombre.Name = "lbl_nombre";
+            this.lbl_nombre.Size = new System.Drawing.Size(44, 13);
+            this.lbl_nombre.TabIndex = 2;
+            this.lbl_nombre.Text = "Nombre";
+            // 
+            // lbl_descripcion
+            // 
+            this.lbl_descripcion.AutoSize = true;
+            this.lbl_descripcion.Location = new System.Drawing.Point(17, 275);
+            this.lbl_descripcion.Name = "lbl_descripcion";
+            this.lbl_descripcion.Size = new System.Drawing.Size(63, 13);
+            this.lbl_descripcion.TabIndex = 3;
+            this.lbl_descripcion.Text = "Descripcion";
+            this.lbl_descripcion.Click += new System.EventHandler(this.lbl_descripcion_Click);
+            // 
+            // lbl_subtitulo
+            // 
+            this.lbl_subtitulo.AutoSize = true;
+            this.lbl_subtitulo.Font = new System.Drawing.Font("Lexend", 10F);
+            this.lbl_subtitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(128)))), ((int)(((byte)(150)))));
+            this.lbl_subtitulo.Location = new System.Drawing.Point(27, 64);
+            this.lbl_subtitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_subtitulo.Name = "lbl_subtitulo";
+            this.lbl_subtitulo.Size = new System.Drawing.Size(235, 22);
+            this.lbl_subtitulo.TabIndex = 5;
+            this.lbl_subtitulo.Text = "Registre las nuevas membresias ";
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Lexend", 16F, System.Drawing.FontStyle.Bold);
+            this.lbl_titulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(44)))));
+            this.lbl_titulo.Location = new System.Drawing.Point(25, 29);
+            this.lbl_titulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(352, 35);
+            this.lbl_titulo.TabIndex = 4;
+            this.lbl_titulo.Text = "Registrar Nuevas Membresias";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_precio);
+            this.panel1.Controls.Add(this.lbl_nombre);
+            this.panel1.Controls.Add(this.lbl_descripcion);
+            this.panel1.Controls.Add(this.lbl_duracion);
+            this.panel1.Location = new System.Drawing.Point(31, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 347);
+            this.panel1.TabIndex = 6;
+            // 
+            // modulo_de_registrar_membresias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 504);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbl_subtitulo);
+            this.Controls.Add(this.lbl_titulo);
+            this.Name = "modulo_de_registrar_membresias";
+            this.Text = "Registrar membresias - Go fitnes";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_precio;
+        private System.Windows.Forms.Label lbl_duracion;
+        private System.Windows.Forms.Label lbl_nombre;
+        private System.Windows.Forms.Label lbl_descripcion;
+        private System.Windows.Forms.Label lbl_subtitulo;
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ GoFitnessGym/modulo_de_registrar_membresias.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFitnessGym
+{
+    public partial class modulo_de_registrar_membresias : Form
+    {
+        public modulo_de_registrar_membresias()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_duracion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_descripcion_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
