bc2d711538ad3e8435a46137dd6481d32c3c53f4
Elvinson Janel Fragoso Rivas <elvinsonjanel12@gmail.com>
2026-06-26 13:50:55 -0400

    Nuevo consulta_de_clientes

---
+++ GoFitnessGym/consulta_de_clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFitnessGym
+{
+    public partial class consulta_de_clientes : Form
+    {
+        public consulta_de_clientes()
+        {
+            InitializeComponent();
+        }
+    }
+}
